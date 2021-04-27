e79c273eec301d07a4e37f27b6259098b427e2f1
anupam0303 <bittu.anupam@gmail.com>
2021-04-27 19:29:40 +0300

    Updated app text

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Here comes the test eshop.
+          Here comes the placeholder for test eshop.
         </p>
       </header>
     </div>
